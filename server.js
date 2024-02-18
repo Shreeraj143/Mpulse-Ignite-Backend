d69c72c38d88aea42bc5e7200aae2543919630c0
Shreeraj Shinde <shreerajs123@gmail.com>
2024-02-18 16:16:07 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,50 +1,45 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import { config } from 'dotenv';
-import router from './router/route.js';
-
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+import router from "./router/route.js";
 
 /** import connection file */
-import connect from './database/conn.js';
+import connect from "./database/conn.js";
 
-const app = express()
-
+const app = express();
 
 /** app middlewares */
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 config();
 
-
 /** appliation port */
 const port = process.env.PORT || 8080;
 
-
 /** routes */
-app.use('/api', router) /** apis */
+app.use("/api", router); /** apis */
 
-
-app.get('/', (req, res) => {
-    try {
-        res.json("Get Request")
-    } catch (error) {
-        res.json(error)
-    }
-})
-
+app.get("/hello", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 /** start server only when we have valid connection */
-connect().then(() => {
+connect()
+  .then(() => {
     try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`)
-        })
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
     } catch (error) {
-        console.log("Cannot connect to the server");
+      console.log("Cannot connect to the server");
     }
-}).catch(error => {
+  })
+  .catch((error) => {
     console.log("Invalid Database Connection");
-})
-
+  });
